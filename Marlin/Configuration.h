40fc80b4e87f5c87efd1e2cd5eaa5b60abfa8406
Vladyslav Kazantsev <specialistvlad@gmail.com>
2021-06-13 16:56:20 +0300

    disable sd card, problems with usb connection

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -121,7 +121,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 250000
+#define BAUDRATE 115200
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
@@ -1656,7 +1656,7 @@
  * you must uncomment the following option or it won't work.
  *
  */
-#define SDSUPPORT
+//#define SDSUPPORT
 
 /**
  * SD CARD: SPI SPEED
